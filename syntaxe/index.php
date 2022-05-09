3129b9095eac5911a3120bdacf8ffdf4031411db
sane cheikh oumar <safietou2717@gmail.com>
2022-05-09 11:31:55 +0200

    declaration de variables

--- syntaxe/index.php
+++ syntaxe/index.php
@@ -14,4 +14,54 @@ echo 'Texte en php','Autre texte ';
 * sur plusieurs
 *ligne 
 */
+
+//variables
+$variable='une variable ';
+$variable ='une autre varriable ';
+echo $variable, $variable;
+//$var = 34;
+$_3var =34;
+echo $_3var;
+
+
+$camelcase =' une variable en camalcase';
+$snake_case =' une variabl en snakecase';
+$pascalCase =' une variable pascale case';
+$var ='une variable en kebab_case';
+
+//string
+$nom =' oumar ';
+//int 
+$nombre =34;
+//bool
+$vrai = true;
+$faux =false;
+
+$tableau =[ 'groupe ',34,45,6 ,false];
+//null
+$variable = null;
+
+//simple /double guillemets (quotes)
+
+//message='"aujourd 'hui";
+//$message = 'aujourd\'hui';
+ $txt = 'Bonjour';
+ echo '$txt tout le mondr<br>';
+ echo "$txt tout le monde";
+
+ //concatention
+ $fruit1 'kiwi';
+ $fruit2 ='Pomme';
+ $fruit3 ='Banane'
+
+ echo $fruit1 .'<br>' .fruit2;
+
+
+
+
+
+
+
 ?>
+
+
