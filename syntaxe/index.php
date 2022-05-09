38e37cb450d5a685615cff2b282e813740fcf678
sane cheikh oumar <safietou2717@gmail.com>
2022-05-09 10:30:31 +0200

    commenetaire en php

---
+++ syntaxe/index.php
@@ -0,0 +1,17 @@
+<h1>un titre<h1>
+
+<?php
+// echo/print
+echo 'Texte en php','Autre texte ';
+//print 'Texte en php avec print<br>' ,
+//'Autre texte avec print'
+
+//un commentaire 
+# un commentaire 
+/*commmentairs
+/* 
+*commmentaire 
+* sur plusieurs
+*ligne 
+*/
+?>
