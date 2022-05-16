30914b3ed07aba6fe75dc95a42ed8fcbee78dd0a
sane cheikh oumar <safietou2717@gmail.com>
2022-05-16 11:07:20 +0200

    heure

--- 04-condition/index.php
+++ 04-condition/index.php
@@ -26,7 +26,7 @@ if($age >=18){
      " vous avez le droit de vote car votre age est de $age ans";
     }
 else{
-    echo 'vous n'\' avez pas le droit ';
+    //echo 'vous n'\' avez pas le droit ';
     echo " vous n'avez pas le droit car votre age est de ' .age ans " ; 
 }
 
@@ -37,5 +37,14 @@ echo <'hr'>;
 $heure =date (H);
 echo $heure.'<br>'
 // avec la  condition if /else afficher si on est le matin ou le soir 
+if ($heure <=12)
+
+    echo "c'est le matin ,il est $heure !";
+    elseif($heure<=16){
+        echo " c'est l'apres midi ,il est $heure !";
+    } else {
+        echo "c'est le soir ,il est $heure !";
+    };
+
 
 ?>
